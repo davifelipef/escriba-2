90955e0be5d828efe96c2f569071bd8835261b5d
davifelipef <feliperedfield@gmail.com>
2026-09-14 18:24:37 -0300

    fix: fixed the path to the icon images

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
         theme_color: "#222222",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/escriba-2/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/escriba-2/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
